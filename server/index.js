5ff592b00d6da429b3dcbeca92199626ff939eab
mailtomainak <mailtomainak@gmail.com>
2018-11-13 10:03:01 +0530

    initial Checkin

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const csvToJson = require('csvtojson');
+const fs = require('fs');
+const websocket = require('websocket-stream')
+const http = require('http');
+
+const slowStream = require('slow-stream');
+
+const server = http.createServer((req,res)=>{
+
+})
+
+const ws = websocket.createServer({
+    server:server,
+    perMessageDeflate: false,
+    objectMode:true
+}, (stream,request)=>{
+    fs.createReadStream('./server/data/DEOK_hourly.csv',{ bufferSize: 64 })
+    .pipe(csvToJson())
+    .pipe(new slowStream ({ maxWriteInterval: 5 })) // replicate real time processing
+    .pipe(stream);
+})
+
+server.listen(8902);
